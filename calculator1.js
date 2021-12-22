9af3be7da8cc9fdab5d57349c07f42801b6ea8ac
SubhenduKumar <bhsubhendukumar66@gmail.com>
2021-12-22 19:08:34 +0530

    Calc

---
+++ calculator1.js
@@ -0,0 +1,24 @@
+var num1 = document.getElementById("num1");
+var num2 = document.getElementById("num2");
+var operator = document.getElementById("operator");
+var result = document.getElementById("result");
+
+function calculator() {
+    if (num1.value === "") {
+        alert("enter no 1")
+    } else if (num2.value === "") {
+        alert("enter no 2")
+    } else {
+        var n1v = Number(num1.value);
+        var n2v = Number(num2.value);
+        var ov = operator.value
+        if (ov === "+") {
+            result.value = n1v + n2v;
+        } else if (ov === "-") {
+            result.value = n1v - n2v;
+        } else if (ov === "*") {
+            result.value = n1v * n2v;
+        } else
+            result.value = n1v / n2v;
+    }
+}
